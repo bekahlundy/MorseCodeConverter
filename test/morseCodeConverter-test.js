4132bcc56526cb078d7da06072e64b4edece72e3
bekahlundy <lundyrebekah@gmail.com>
2017-05-04 14:30:15 -0600

    Update tests

--- test/morseCodeConverter-test.js
+++ test/morseCodeConverter-test.js
@@ -6,12 +6,15 @@ describe('morseCodeConverter', () => {
   it('returns HEY JUDE', () => {
     assert.deepEqual(morseCodeConverter('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
   })
+
   it('returns SOS', () => {
     assert.deepEqual(morseCodeConverter('...---...'), 'SOS')
   })
+
   it('returns SOS HEY JUDE', () => {
     assert.deepEqual(morseCodeConverter('...---...   .... . -.--   .--- ..- -.. .'), 'SOS HEY JUDE')
   })
+  
   it('returns SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', () => {
     assert.deepEqual(morseCodeConverter('... --- ... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-'), 'SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG')
   })
